4849b1a5ebaa34cfb5dd04426f00b299ff83a1cb
Hevon Gordon <hevondgordon.professional@gmail.com>
2021-10-30 21:52:14 -0500

    added db config

---
+++ api/pyrate/pyrate/db.py
@@ -0,0 +1,7 @@
+from app import app
+
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
