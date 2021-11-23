1ef047cb517fcf7c346d9988e20fc9fd80d1fed9
Hevon Gordon <hevondgordon.professional@gmail.com>
2021-11-23 01:23:03 -0500

    generated new user service with model definitions

--- api/pyrate/pyrate/services/user/model.py
+++ api/pyrate/pyrate/services/user/model.py
@@ -1,24 +1,24 @@
+
 from db import db
 from services.__internals__.models import PyrateBaseModel
 
 
 class User(db.Model, PyrateBaseModel):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), nullable=False)
-    last_name = db.Column(db.String(80), nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False, unique=True)
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
-
+        return self
+    
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
 
     def __repr__(self):
-        name = '{first_name} {last_name} - ({email})'.format(
-            first_name=self.first_name, last_name=self.last_name, email=self.email)
-        return 'User: {name}'.format(name=name)
+        return self.id
--- api/pyrate/pyrate/services/user/routes.py
+++ api/pyrate/pyrate/services/user/routes.py
@@ -1,23 +1,25 @@
+
 def routes():
     return [
         {'methods': ['GET'],
-         'path': '/',
-         'controller': 'find_user'
-         },
+        'path': '/',
+        'controller': 'find_user'
+        },
         {'methods': ['GET'],
-         'path': '/<int:id>',
-         'controller': 'find_single_user'
-         },
+        'path': '/<int:id>',
+        'controller': 'find_single_user'
+        },
         {'methods': ['POST'],
-         'path': '/',
-         'controller': 'create_user'
-         },
+        'path': '/',
+        'controller': 'create_user'
+        },
         {'methods': ['PUT'],
-         'path': '/<int:id>',
-         'controller': 'update_user'
-         },
+        'path': '/<int:id>',
+        'controller': 'update_user'
+        },
         {'methods': ['DELETE'],
-         'path': '/<int:id>',
-         'controller': 'delete_user'
-         },
+        'path': '/<int:id>',
+        'controller': 'delete_user'
+        },
     ]
+    
