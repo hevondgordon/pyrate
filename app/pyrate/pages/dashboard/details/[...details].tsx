6cc49134aa339d3ff6bca0bb03cbb962605b340f
Hevon Gordon <hevondgordon.professional@gmail.com>
2021-11-30 08:51:54 -0500

    added generic form to details page

--- app/pyrate/pages/dashboard/details/[...details].tsx
+++ app/pyrate/pages/dashboard/details/[...details].tsx
@@ -1,15 +1,24 @@
 import { useRouter } from 'next/router'
+import { fetcher } from '../../../data/utils'
+import { ROUTE as GET_COLUMN_DETAILS } from '../../api/services/getColumnDetails'
 import { Card } from 'antd';
 import useSWR from 'swr'
 import GenericForm from '../../../components/GenericForm'
 
+
 export default function EntityDetail() {
     const router = useRouter()
-    const { entity, id } = router.query
+    const { entity, id, details } = router.query
+    const {
+        data: columnDetails,
+    } = useSWR(`${GET_COLUMN_DETAILS}?model=${details?.[0]}`, fetcher)
 
     return (
         <Card title="Default size card" extra={<a href="#">More</a>}>
-            <GenericForm fields={[{ label: 'this is the label', type: 'this is the type' }]} />
+            <GenericForm
+                columns={columnDetails?.data?.columns}
+            />
         </Card>
+
     );
 }
