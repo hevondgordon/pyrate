c895a7764c20301278508e5fae49473f7c041cca
Hevon Gordon <hevondgordon.professional@gmail.com>
2021-11-30 08:56:38 -0500

    adding alembic to project

--- api/pyrate/pyrate/app.py
+++ api/pyrate/pyrate/app.py
@@ -1,3 +1,4 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- api/pyrate/pyrate/db.py
+++ api/pyrate/pyrate/db.py
@@ -1,7 +1,7 @@
 from app import app
-
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
