7df12602d4fb3d6810abdd7231f04fa95c3f2a2a
Hevon Gordon <hevondgordon.professional@gmail.com>
2021-11-30 08:52:12 -0500

    updated how column data is fetched

--- app/pyrate/pages/dashboard/[serviceName].tsx
+++ app/pyrate/pages/dashboard/[serviceName].tsx
@@ -1,37 +1,36 @@
-import GenericTable from "../../components/GenericTable";
-import GenericModal from "../../components/GenericModal";
-import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { ROUTE as GET_SERVICE_ROUTE } from '../api/services/[serviceType]'
-import { ROUTE as DELETE_SERVICE_ROUTE } from '../api/services/delete'
+import { ROUTE as GET_COLUMN_DETAILS } from '../api/services/getColumnDetails'
 import { fetcher, handleServiceDelete } from '../../data/utils'
+import { _Column } from "../../types";
+import GenericTable from "../../components/GenericTable";
 import useSWR from 'swr'
 
-
-const getTableColumns = (services: any) => {
-    let tableColumns: string[] = [];
-    const service = services?.[0];
-
-    if (service) {
-        tableColumns = Object.keys(service)
-    }
-    return tableColumns;
-}
 
 export default function DashboardContent() {
     const router = useRouter()
     const { serviceName } = router.query
-    const { data, error, mutate: refetchServices, } = useSWR(GET_SERVICE_ROUTE(serviceName as string), fetcher)
+    const {
+        data: tableData,
+        mutate: refetchServices
+    } = useSWR(GET_SERVICE_ROUTE(serviceName as string), fetcher)
 
-    const tableColumns = getTableColumns(data)
+    const {
+        data: columnDetails,
+    } = useSWR(`${GET_COLUMN_DETAILS}?model=${serviceName}`, fetcher)
+
+    const columnTitles: string[] = columnDetails?.data?.columns.map((column: _Column) => column.name);
 
     return (
         <div>
             <GenericTable
                 refetch={refetchServices}
-                columns={tableColumns}
-                dataSource={data}
+                columns={columnTitles}
+                dataSource={tableData}
                 serviceName={serviceName as string} />
         </div>
     )
 }
+
+
+
