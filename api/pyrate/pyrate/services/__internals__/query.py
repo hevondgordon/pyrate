ff6cb36c6b7177ae840a6f52f1ce72dc39afefbb
Hevon Gordon <hevondgordon.professional@gmail.com>
2021-12-23 12:34:54 -0500

    MVP: added basic query setup

---
+++ api/pyrate/pyrate/services/__internals__/query.py
@@ -0,0 +1,188 @@
+import psycopg2
+from services.__internals__.db_config import config_string
+
+
+def connect_to_db():
+    """
+    Connect to the database
+    """
+    _config_string = config_string()
+    conn = psycopg2.connect(_config_string)
+    conn.autocommit = True
+    return conn
+
+
+def execute_query(query):
+    """
+    executes a query against a postgres database
+    """
+
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+    return cursor
+
+
+def create_model(model_name, model_fields):
+    """
+    Create a model in the database
+    """
+    query = """
+        CREATE TABLE IF NOT EXISTS {} (
+            id SERIAL PRIMARY KEY,
+            {}
+        );
+    """.format(model_name, create_model_fields(model_fields))
+    cursor = execute_query(query)
+    cursor.close()
+
+
+def create_model_fields(model_fields):
+    """
+    creates postgres fields based on the provided model_fields
+    """
+    _model_fields = [create_model_field(model_field)
+                     for model_field in model_fields]
+    return ',\n'.join(_model_fields)
+
+
+def create_model_field(model_field_data: dict):
+    """
+    creates a single postgres field
+    """
+
+    field_name = model_field_data.get('name')
+    field_type = model_field_data.get('type')
+    nullable = model_field_data.get('nullable', '')
+    default = model_field_data.get('default', '')
+    unique = model_field_data.get('unique', '')
+    field_length = model_field_data.get('field_length', None)
+
+    if nullable.lower() == 'false':
+        nullable = 'NOT NULL'
+
+    if unique == 'true':
+        unique = 'UNIQUE'
+
+    if default == 'true':
+        default = "DEFAULT '{}'".format(default)
+
+    if field_length is None:
+        field_length = 255
+
+    if field_type == 'varchar':
+        field_type = 'varchar({})'.format(field_length)
+
+    return f"{field_name} {field_type} {nullable} {default} {unique}".strip()
+
+
+def format_data_values(data: dict):
+    """
+    formats data values for db
+    """
+    param_values = data.values()
+
+    string_values = [
+        f"'{param}'" for param in param_values if isinstance(param, str)]
+
+    non_string_values = [param for param in param_values if isinstance(
+        param, str) == False]
+
+    return string_values + non_string_values
+
+
+def format_data_into_expresion_for_db(data: dict):
+    """
+    formats data into expression for db
+    """
+    param_names = data.keys()
+
+    string_values = [
+        f"{param} = '{data[param]}'" for param in param_names if isinstance(data[param], str)]
+
+    non_string_values = [f"{param} = {data[param]}" for param in param_names if isinstance(
+        data[param], str) == False]
+
+    return string_values + non_string_values
+
+
+def create(model_name: str, data: dict):
+    """
+    Create a model in the database
+    """
+
+    columns = ',\n'.join(data.keys())
+    values = ',\n'.join(format_data_values(data))
+    query = """
+        INSERT INTO {} (
+            {}
+        ) VALUES (
+            {}
+        );
+    """.format(model_name, columns, values)
+    with execute_query(query) as cursor:
+        return cursor.fetchall()
+
+
+def get_all(model_name):
+    """
+    Get a model from the database
+    """
+    query = """
+        SELECT * FROM {};
+    """.format(model_name)
+
+    with execute_query(query) as cursor:
+        return cursor.fetchall()
+
+
+def filter(model_name: str, filter_data: dict):
+    """
+    Filter a model from the database
+    """
+
+    formatted_data = format_data_into_expresion_for_db(filter_data)
+    filter_param_string = ' AND\n'.join(formatted_data)
+
+    query = """
+        SELECT * FROM {} WHERE {};
+    """.format(model_name, filter_param_string)
+
+    with execute_query(query) as cursor:
+        return cursor.fetchall()
+
+
+def update(model_name: str, filter_data: dict, update_data: dict):
+    """
+    Update a model in the database
+    """
+
+    formatted_data = format_data_into_expresion_for_db(update_data)
+    formatted_filter_data = format_data_into_expresion_for_db(filter_data)
+    update_data_string = ', '.join(formatted_data)
+    filter_data_string = ' AND '.join(formatted_filter_data)
+
+    query = """
+        UPDATE {} SET {} WHERE {};
+    """.format(model_name, update_data_string, filter_data_string)
+
+    with execute_query(query) as cursor:
+        return cursor.fetchall()
+
+
+def delete(model_name: str, filter_data: dict):
+    """
+    Delete a model from the database
+    """
+
+    formatted_data = format_data_into_expresion_for_db(filter_data)
+    filter_data_string = ' AND '.join(formatted_data)
+
+    query = """
+        DELETE FROM {} WHERE {};
+    """.format(model_name, filter_data_string)
+
+    with execute_query(query) as cursor:
+        return cursor.fetchall()
