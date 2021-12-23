e8560318b6ba381fc1b149926e98397840aca542
Hevon Gordon <hevondgordon.professional@gmail.com>
2021-12-23 12:34:19 -0500

    MVP: added local config file

---
+++ api/pyrate/pyrate/services/__internals__/db_config.py
@@ -0,0 +1,11 @@
+def config():
+    return {
+        'host': 'localhost',
+        'port': 5432,
+        'user': 'pyrate',
+        'password': 'pyrate',
+        'db': 'pyrate'
+    }
+
+def config_string():
+    return 'postgresql://{user}:{password}@{host}:{port}/{db}'.format(**config())
