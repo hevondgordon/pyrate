c3cd6d1ea540cbfea87bd4f3f771367343d8b944
Hevon Gordon <hevondgordon.professional@gmail.com>
2022-03-02 23:03:51 -0500

    containerizing API portion application

--- api/pyrate/pyrate/services/__internals__/db_config.py
+++ api/pyrate/pyrate/services/__internals__/db_config.py
@@ -1,9 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print("loaded", os.getenv("HOST"))
+
 CONFIG = {
-    "host": "localhost",
-    "port": 5432,
-    "user": "pyrate",
-    "password": "pyrate",
-    "db": "pyrate",
+    "host": os.getenv("HOST"),
+    "port": os.getenv("PORT"),
+    "user": os.getenv("USER"),
+    "password": os.getenv("PASSWORD"),
+    "db": os.getenv("DB"),
 }
 
 
